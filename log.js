a42e5b4c22c190814e280bfad40cb35b3b85c978
lukebarnard <luke.barnard99@gmail.com>
2020-02-23 19:48:11 +0000

    Disable logging by default

--- log.js
+++ log.js
@@ -12,12 +12,17 @@ try {
 
 const stream = fs.createWriteStream(logPath, { flags: 'a' })
 
-const log = {
+const { NOMAD_LOGGING } = process.env
+
+const log = NOMAD_LOGGING === 'true' ? {
   info: (o) => stream.write(JSON.stringify(o) + '\n'),
   error: (e) => stream.write(JSON.stringify({
     message: e.message,
     stack: e.stack.split('\n')
   }) + '\n')
+} : {
+  info: () => {},
+  error: () => {}
 }
 
 module.exports = log
